cbf952ad52d7f7472a72482cb0b94f67cb66b621
EternalJew <mabuttisonfire@gmail.com>
2021-03-10 20:38:24 +0200

    Done

--- 1.1/main.js
+++ 1.1/main.js
@@ -1,11 +1,9 @@
-const summ = require('./summ');
-const min = require('./min');
-const abs = require('./abs');
+const math = require('./math/index');
 let array = [
     4, 33, 23, 53, 13, 3, 88, 3, 5
 ]
 
 let num = -5;
-console.log(abs.abs(num))
-console.log(min.min(array))
-console.log(summ.summ(array))
+console.log(math.abs(num))
+console.log(math.min(array))
+console.log(math.summ(array))
